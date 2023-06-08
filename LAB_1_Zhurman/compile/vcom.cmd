f68777032bebc6a50ebd6e0e0b7307bbbf035e96
AlexCo01 <125486902+AlexCo01@users.noreply.github.com>
2023-06-09 01:11:06 +0300

    Add files via upload

---
+++ LAB_1_Zhurman/compile/vcom.cmd
@@ -0,0 +1,1 @@
+-w -erf c:/My_Designs/LAB_1_Zhurman/LAB_1_Zhurman/compile/LAB_1_Zhurman.erf -d c:/My_Designs/LAB_1_Zhurman/LAB_1_Zhurman/compile -s c:/My_Designs/LAB_1_Zhurman/library.cfg -j c:/My_Designs/LAB_1_Zhurman/LAB_1_Zhurman/projlib.cfg -work LAB_1_Zhurman -O3 -e 100 -work LAB_1_Zhurman -2002 c:/My_Designs/LAB_1_Zhurman/LAB_1_Zhurman/src/led_driver_tb.vhd 
